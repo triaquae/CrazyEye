113215beb6c9c28bfc184d29add2502d200e726c
triaquae <triaquae@gmail.com>
2016-03-24 11:35:32 +0800

    Update settings.py

--- CrazyEye/settings.py
+++ CrazyEye/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'suit',
+    #'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
