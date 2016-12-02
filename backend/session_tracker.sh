88b7f71a61543c3bc98a60bbd848f9c3044cd7a5
Alex Li <lijie3721@126.com>
2016-12-02 10:48:00 +0800

    修正日志记录脚本bug

--- backend/session_tracker.sh
+++ backend/session_tracker.sh
@@ -9,7 +9,7 @@ session_tag=$2
 
 for i in $(seq 1 30)
 do
-    has_process=`ps -ef |grep ssh|grep $session_tag |awk 'NR==1{ print $2 }'`
+    has_process=`ps -ef |grep ssh|grep -v sshpass|grep $session_tag |awk '{ print $2 }'`
     if [ "$has_process" = "" ]
     then
         sleep 1
@@ -27,7 +27,7 @@ do
             mkdir $today_audit_dir
         fi;
         #ps -ef |awk '{ print $2 }'  |grep $has_process > "logs/audit/$today/session_$1.log"
-        sudo strace  -fp 6753 -t -o  "logs/audit/$today/session_$1.log"
+        sudo strace  -fp $has_process -t -o  "logs/audit/$today/session_$1.log"
         break;
     fi;
 
